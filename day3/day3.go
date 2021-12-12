19f5ff90afb5cffe60fc392cf7876925df4a8756
Dimitri Klimenko <d.klimenko@arkoselabs.com>
2021-12-12 18:08:23 +1000

    Day 3 Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 
 	"github.com/dimitriklimenko/advent-of-code-2021/common"
 )
@@ -37,11 +38,69 @@ func calcPowerConsumption(report []string) int {
 	return gammaRate * epsilonRate
 }
 
+func filterByBit(report []string, index int, keepMostCommon bool) []string {
+	zeros := []string{}
+	ones := []string{}
+	for _, number := range report {
+		if number[index] == '0' {
+			zeros = append(zeros, number)
+		} else {
+			ones = append(ones, number)
+		}
+	}
+
+	if len(zeros) > len(ones) {
+		if keepMostCommon {
+			return zeros
+		} else {
+			return ones
+		}
+	} else {
+		if keepMostCommon {
+			return ones
+		} else {
+			return zeros
+		}
+	}
+}
+
+func calcRating(report []string, keepMostCommon bool) uint64 {
+	// Assume all numbers have the same # of bits.
+	numBits := len(report[0])
+
+	currentIndex := 0
+
+	for len(report) > 1 {
+		if currentIndex >= numBits {
+			panic("Non-unique numbers; could not determine a rating!")
+		}
+		report = filterByBit(report, currentIndex, keepMostCommon)
+		currentIndex += 1
+	}
+
+	if len(report) == 0 {
+		panic("Least common value occurred zero times!")
+	}
+
+	rating, _ := strconv.ParseUint(report[0], 2, 64)
+	return rating
+}
+
+func calcLifeSupportRating(report []string) uint64 {
+	return calcRating(report, true) * calcRating(report, false)
+}
+
 func main() {
 	filePath := flag.String("f", "", "file to read from")
+	isLifeSupport := flag.Bool("life", true, "set to calculat life support rating")
 	flag.Parse()
 
 	report := common.ParseStrings(filePath)
-	powerConsumption := calcPowerConsumption(report)
-	fmt.Println(powerConsumption)
+	if *isLifeSupport {
+		lifeSupportRating := calcLifeSupportRating(report)
+		fmt.Println(lifeSupportRating)
+	} else {
+		powerConsumption := calcPowerConsumption(report)
+		fmt.Println(powerConsumption)
+	}
 }
